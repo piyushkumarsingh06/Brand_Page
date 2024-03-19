ca315dcb7a6676021339900b6c4b5286578fdeeb
piyush kumar singh <p262726@gmail.com>
2024-03-19 14:36:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+const App = () => {
+  return <div>
+<Nav></Nav>
+<Hero></Hero>
+    </div>;
+};
+
+export default App;
